9ffd24388f3360b6801071a26b6262a0e7b4a63e
Alexander Ng <an@asu.edu>
2024-04-06 19:12:35 -0700

    Moved into java src.

--- app/src/main/java/edu/asu/cse360s24/Patient.java
+++ app/src/main/java/edu/asu/cse360s24/Patient.java
@@ -1,10 +1,10 @@
-package epicmedical;
+package edu.asu.cse360s24;
 
 import java.util.Random;
 import java.util.ArrayList;
 
 public class Patient {
-    
+
     private String patientId = "";
     private String firstName = "";
     private String lastName = "";
@@ -13,9 +13,10 @@ public class Patient {
     private int phoneNumber = 0;
     private String password = "";
 
-	private ArrayList<Message> messages;
-    
-    public Patient(String patientId, String firstName, String lastName, String dateOfBirth, int age, ArrayList<Message> messages, int phoneNumber, String password) {
+    private ArrayList<Message> messages;
+
+    public Patient(String patientId, String firstName, String lastName, String dateOfBirth, int age,
+            ArrayList<Message> messages, int phoneNumber, String password) {
         this.patientId = patientId;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -25,8 +26,9 @@ public class Patient {
         this.password = password;
         this.messages = messages;
     }
-    
-    public Patient(String patientId, String firstName, String lastName, String dateOfBirth, int age, int phoneNumber, String password) {
+
+    public Patient(String patientId, String firstName, String lastName, String dateOfBirth, int age, int phoneNumber,
+            String password) {
         this.patientId = patientId;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -36,64 +38,63 @@ public class Patient {
         this.password = password;
         this.messages = new ArrayList<Message>();
     }
-    
-    private void setPhoneNumber(int phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	
-    private int getPhoneNumber() {
-		return phoneNumber;
-	}
 
-	private void setPassword(String password) {
-		this.password = password;
-	}
-	
-	private String getPassword() {
-		return password;
-	}
+    private void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String getPassword() {
+        return password;
+    }
 
     private void generatePatientID() {
         Random random = new Random(System.currentTimeMillis());
         int id = ((1 + random.nextInt(2)) * 10000 + random.nextInt(10000));
         this.patientId = String.valueOf(id);
     }
-    
+
     public String getPatientID() {
         return this.patientId;
     }
-   
-	
-	private void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	private String getFirstName() {
-		return this.firstName;
-	}
-	
-	private void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	private String getLastName() {
-		return this.lastName;
-	}
-	
-	private void setDateofBirth(String dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-	
-	private String getDateofBirth() {
-		return this.dateOfBirth;
-	}
-	
-	private void setAge(int age) {
-		this.age = age;
-	}
-	
-	private int getAge() {
-		return this.age;
-	}
-	
+
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String getFirstName() {
+        return this.firstName;
+    }
+
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String getLastName() {
+        return this.lastName;
+    }
+
+    private void setDateofBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    private String getDateofBirth() {
+        return this.dateOfBirth;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+
+    private int getAge() {
+        return this.age;
+    }
+
 }
