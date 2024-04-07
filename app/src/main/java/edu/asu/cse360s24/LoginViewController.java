5a1965c5115641d351e2ef27f5b4e2b964785fdb
arcxdia <117484521+arcxdia@users.noreply.github.com>
2024-04-06 21:57:30 -0700

    interface shells

--- app/src/main/java/edu/asu/cse360s24/LoginViewController.java
+++ app/src/main/java/edu/asu/cse360s24/LoginViewController.java
@@ -11,6 +11,9 @@ public class LoginViewController extends AbstractController {
 
 	@FXML
 	PasswordField passwordTxt;
+	
+	@FXML
+	TextField messageText;
 
 	@FXML
 	protected void submitLogin(ActionEvent evt) {
@@ -24,20 +27,81 @@ public class LoginViewController extends AbstractController {
 	protected void doctorLogin(ActionEvent evt) {
 		app.loadScene("DoctorLogin.fxml");
 	}
+	
+	@FXML
+	protected void doctorPortal(ActionEvent evt) {
+		app.loadScene("DoctorPortal.fxml");
+	}
 
 	@FXML
 	protected void nurseLogin(ActionEvent evt) {
 		app.loadScene("NurseLogin.fxml");
 	}
+	
+	@FXML
+	protected void nursePortal(ActionEvent evt) {
+		app.loadScene("NursePortal.fxml");
+	}
 
 	@FXML
 	protected void patientLogin(ActionEvent evt) {
-		System.out.println("Switching to Patient Login!");
-		// app.loadScene("PatientLogin.fxml");
+		//System.out.println("Switching to Patient Login!");
+		app.loadScene("PatientLogin.fxml");
 	}
 
 	@FXML
 	protected void homeLogin(ActionEvent evt) {
 		app.loadScene("Login.fxml");
 	}
+	
+	@FXML
+	protected void patientPortal(ActionEvent evt) {
+		app.loadScene("PatientPortal.fxml");
+	}
+	
+	@FXML
+	protected void saveInfo(ActionEvent evt) {
+		System.out.println("Saving Info");
+	}
+	
+	@FXML
+	protected void patientMessages(ActionEvent evt) {
+		app.loadScene("PatientMessaging.fxml");
+	}
+	
+	@FXML
+	protected void sendMessage(ActionEvent evt) {
+		System.out.println("Sending message:" + messageText.getText());
+	}
+	
+	@FXML
+	protected void signup(ActionEvent evt) {
+		app.loadScene("PatientSignup1.fxml");
+	}
+	
+	@FXML
+	protected void nextSignup(ActionEvent evt) {
+		app.loadScene("PatientSignup2.fxml");
+	}
+	
+	@FXML
+	protected void newVisit(ActionEvent evt) {
+		app.loadScene("NewVisit.fxml");
+	}
+	
+	@FXML
+	protected void nurseMessages(ActionEvent evt) {
+		app.loadScene("NurseMessaging.fxml");
+	}
+	
+	@FXML
+	protected void contVisit(ActionEvent evt) {
+		app.loadScene("ContinueVisit.fxml");
+	}
+	
+	@FXML
+	protected void doctorMessages(ActionEvent evt) {
+		app.loadScene("DoctorMessaging.fxml");
+	}
+	
 }
